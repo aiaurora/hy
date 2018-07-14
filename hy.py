37ad461bc41384f0f4f6d6d25b5121e1af5938c8
aiaurora <41220173+aiaurora@users.noreply.github.com>
2018-07-14 13:14:10 +0800

    v2

--- hy.py
+++ hy.py
@@ -1,2 +1,8 @@
 name=input("姓名:")
-print('嗨2',name)
+print('嗨!',name)
+a = int(input('今天有沒有下雨? 0：無，1：有'))
+if a == 1:
+    print('留在家裡吧!')
+b = float(input('今天氣溫攝氏幾度?'))
+b = b * 9 / 5 + 32
+print('華氏:', b, '度')
